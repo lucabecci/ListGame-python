cf124f8ef11c86e83c691d6565670548a39df109
luca <beccibrian@gmail.com>
2020-10-12 19:51:02 -0300

    feat: game finish.

---
+++ listgame.py
@@ -0,0 +1,10 @@
+dato= input('Insert your date: ')
+
+lista = ['hello', 'world', 'piggy', 'happy', 'serpents']
+
+if lista.count(dato) > 0:
+    print('Your input exists:', dato)
+    print('YESSSSSSSSSSSSS, YOU ARE A WINNER')
+else:
+    print('Your input not exists', dato)
+    print('OH SOOOO SADDDDD, YOU ARE A LOSER. TRY AGAIN BOY')
